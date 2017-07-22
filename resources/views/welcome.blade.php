8cda6d4622e9eaae6325dfb1ee0bb9156de2ad6e
Jian James Astrero <jianjamesastrero@gmail.com>
2017-07-22 16:29:25 +0800

    Frontend

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,95 +1,79 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<head>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/main.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/font-awesome.css') }}">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css"
+          type="text/css">
+    <style>
+        .onepage {
+            width: 100%;
+            height: 100vh;
+            overflow-x: hidden;
+        }
+        .backmaps {
+            background: #f0f;
+            width: 100%;
+            height: 100%;
+            position: absolute;
+            z-index: -500;
+        }
+        .list {
+            background-color: #dedede;
+            -webkit-border-radius: 4px;
+            -moz-border-radius: 4px;
+            border-radius: 4px;
+            left: 8px;
+            height: 70%;
+        }
+    </style>
+</head>
 
-        <title>Laravel</title>
+<body>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+<!--TOP-->
+<div class="onepage">
+    <header>
+        <h1><span>LINK APP</span>
+            <br>
+            <p>a social app</p>
+            <div class="flex">
+                <a href="#" class="bttn" style="margin-right: 10%">About</a>
+                <a href="#" class="bttn">Login</a>
             </div>
-        </div>
-    </body>
+        </h1>
+
+        <canvas style="overflow-x: hidden"></canvas>
+    </header>
+</div>
+
+<!--LIST-->
+<div class="onepage">
+    <div class="backmaps"></div>
+    <h2><span class="title-main">EVACUATION LIST AND MAPS</span></h2>
+    <div class="col-md-4 list">
+        <table class="table">
+            <thead>
+            <tr><th class="col-md-4">LISTS</th><th>MAP</th></tr>
+            </thead>
+            <tbody>
+            <tr>
+                <td>1</td><td>MAP</td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</div>
+
+
+
+
+
+</body>
+<script type="text/javascript" src="{{ asset('js/main.js') }}"></script>
+<script>
+
+</script>
+
 </html>
